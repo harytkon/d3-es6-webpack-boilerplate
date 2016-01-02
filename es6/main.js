ebed55a4cc127822a1f36c9d396ca2f233de58a1
Hannu Rytkönen <hannu.rytkonen@gmail.com>
2016-01-02 18:12:28 +0200

    first commit

---
+++ es6/main.js
@@ -0,0 +1,3 @@
+import d3Graph from './d3Graph.js';
+let graph = new d3Graph('#graph');
+graph.drawRectAngle();
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './es6/main.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: path.join(__dirname, 'es6'),
+                query: {
+                  presets: 'es2015',
+                },
+            }
+        ]
+    },
+    plugins: [
+        // Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin()
+    ],
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    // Create Sourcemaps for the bundle
+    devtool: 'source-map',
+};
